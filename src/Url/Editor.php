db81b7bd0da993af4b788fb28c165510ae042cb3
lucap <luc@rissc.de>
2022-05-02 09:01:17 +0200

    fix editor callback urls

--- src/Url/Editor.php
+++ src/Url/Editor.php
@@ -16,9 +16,10 @@ final class Editor extends Auth
 {
     public function draft(string|Draft $draft, ?string $callback = null, ?string $callback_cancel = null, ?string $callback_halt = null): self
     {
+        $callbacks = array_map('base64_encode', array_filter(compact('callback', 'callback_cancel', 'callback_halt'), static fn(?string $value) => !empty($value)));
         $this->tokenBuilder->redirect = (new Uri($this->config->get('base_uri')))
             ->withPath(sprintf('/editor/%s', static::unwrapResource($draft)))
-            ->withQuery(self::buildQuery(compact('callback', 'callback_cancel', 'callback_halt')));
+            ->withQuery(self::buildQuery($callbacks));
         $this->tokenBuilder->withJTI = true;
         return $this;
     }
