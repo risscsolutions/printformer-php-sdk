a52f4de70a0878e9d189e239bea92b609dfc8d06
lucap <luc@rissc.de>
2022-09-16 08:34:00 +0200

    Add test for show MasterTemplate

--- tests/Client/MasterTemplate/ClientTest.php
+++ tests/Client/MasterTemplate/ClientTest.php
@@ -14,6 +14,7 @@ use GuzzleHttp\Psr7\Response;
 use PHPUnit\Framework\TestCase;
 use Psr\Http\Message\RequestInterface;
 use Rissc\Printformer\Client\MasterTemplate\Client;
+use Rissc\Printformer\Client\MasterTemplate\GroupMember;
 use Rissc\Printformer\Client\MasterTemplate\MasterTemplate;
 use Rissc\Printformer\Client\MasterTemplate\MasterTemplateClient;
 use Rissc\Printformer\Tests\Client\TestsHTTPCalls;
@@ -66,7 +67,7 @@ class ClientTest extends TestCase
         $request = head($container)['request'];
 
         static::assertEquals('GET', $request->getMethod());
-        static::assertEquals ('https://printformer.test/api-ext/template?page=1&per_page=25', (string)$request->getUri());
+        static::assertEquals('https://printformer.test/api-ext/template?page=1&per_page=25', (string)$request->getUri());
         static::assertCount(1, $masterPaginator->getData());
         static::assertTrue($masterPaginator->isLast());
         /** @var MasterTemplate $master */
@@ -74,5 +75,50 @@ class ClientTest extends TestCase
         static::assertEquals('xyzabc12', $master->identifier);
         static::assertEquals('Test Master', $master->name);
         static::assertEquals('now', $master->updatedAt);
+    }
+
+    public function testShow(): void
+    {
+        $container = [];
+        $http = $this->createMockHTTPClient($container, [
+            new Response(200, [], json_encode([
+                'data' => [
+                    'identifier' => 'xyzabc12',
+                    'name' => 'Test Master',
+                    'intents' => ['customize'],
+                    'pageCount' => 1,
+                    'availTemplate' => null,
+                    'correctionTemplate' => [
+                        'name' => 'Test Avail',
+                        'pageCount' => 1
+                    ],
+                    'groupMembers' => [
+                        [
+                            'identifier' => 'j2nd672w',
+                            'name' => 'Test Member',
+                        ]
+                    ],
+                    'customAttributes' => [],
+                    'updatedAt' => 'now',
+                ],
+            ])),
+        ]);
+
+        $client = static::createAPIClient($http);
+        $master = $client->show('xyzabc12');
+
+        static::assertCount(1, $container);
+
+        /** @var RequestInterface $request */
+        $request = head($container)['request'];
+
+        static::assertEquals('GET', $request->getMethod());
+        static::assertEquals('https://printformer.test/api-ext/template/xyzabc12', (string)$request->getUri());
+
+        static::assertEquals('xyzabc12', $master->identifier);
+        static::assertEquals('Test Master', $master->name);
+        static::assertEquals('now', $master->updatedAt);
+        static::assertCount(1, $master->groupMembers);
+        static::assertInstanceOf(GroupMember::class, $master->groupMembers[0]);
     }
 }
